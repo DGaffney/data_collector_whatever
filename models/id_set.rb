b8e05a042eda4e2bf11b3f88e8170c1866ba3b9c
Devin Gaffney <itsme@devingaffney.com>
2014-12-17 09:35:07 -0500

    updates

--- models/id_set.rb
+++ models/id_set.rb
@@ -5,4 +5,12 @@ class IdSet
   key :direction, String
   key :next_cursor, Integer
   key :previous_cursor, Integer
+  
+  def self.followers_for(ids)
+    self.where(direction: "follower", user_id: ids).collect(&:ids).flatten.uniq
+  end
+
+  def self.friends_for(ids)
+    self.where(direction: "friend", user_id: ids).collect(&:ids).flatten.uniq
+  end
 end
